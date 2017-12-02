01e2e45dd0d63a11d78810cb876cc5f5ffd88361
timlindquist <timmylindquist@gmail.com>
2017-12-02 00:35:48 -0600

    main functions
    explanitory

--- enigma.c
+++ enigma.c
@@ -10,52 +10,96 @@ char rotor3[2][26]={{'X','C','U','J','M','I','K','P','S','Y','L','F','R','O','N'
 int reflector[2][26]={{'O','Y','C','R','E','U','B','Z','N','X','S','V','H','W','A','K','Q','I','D','J','M','P','L','F','T','G'},
 										{'U','G','H','A','N','O','I','Q','E','K','J','L','C','M','R','X','Z','B','P','S','W','D','V','T','F','Y'}};
 
+void new_page();
+void rotate(int rotor, int amount);
+void print_array(int array[]);
 int position(char input);
-
 
 int main(){
 	int r1_pos;
 	int r2_pos;
 	int r3_pos;
 	int i,j,k;
-	char input[10];
-	char word[10];
+	char input[10000];
 	printf("Rotor Placement\n");
 	printf("Rotor 1: ");
-	fgets(input,10,stdin);
-	r1_pos=position(input[0]);
+	scanf("%d",&r1_pos);
+	rotate(1,r1_pos);
 	printf("Rotor 2: ");
-	fgets(input,10,stdin);
-	r2_pos=position(input[0]);
+	scanf("%d",&r2_pos);
+	rotate(2,r2_pos);
 	printf("Rotor 3: ");
-	fgets(input,10,stdin);
-	r3_pos=position(input[0]);
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	printf("Key:%d,%d,%d\n",r3_pos,r2_pos,r1_pos);
- int length;
+	scanf("%d",&r3_pos);
+	rotate(3,r3_pos);
+	new_page();
+	printf("\nKey:%d, %d, %d\n",r3_pos,r2_pos,r1_pos);
+
 	for(;;){
-		int letter=0;
-		scanf("%s",word);
-		//length=strlen(word);
-		//printf("%s  %d",word,length);
-		for(k=r3_pos;k<26;k++){		//rotor3
-			for(j=r2_pos;j<26;j++){		//rotor2
-				for(i=r1_pos;i<26;i++){		//rotor1
-					position(word[letter]);
-					letter++;
-				}
-				r1_pos=0;
-			}
-			r2_pos=0;
+		scanf("%s",&input);
+		position(input);
+		while(input[i]!=\0){
+			
 		}
-		r3_pos=0;
+
 	}
+	/*
+	rotate(3,1);
+	for(i=0;i<2;i++){
+		for(j=0;j<26;j++){
+			printf("%c",rotor3[i][j]);
+		}
+		printf("\n");
+	}
+*/
 
 
 return 0;
 }
 
 
+void new_page(){
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
+
+
+void rotate(int rotor, int amount){
+	int i,j,k;
+	char temp;
+	if(rotor==1){
+		for(i=0;i<2;i++){
+			for(j=0;j<amount;j++){
+					temp=rotor1[i][0];
+				for(k=0;k<25;k++){
+					rotor1[i][k]=rotor1[i][k+1];
+					}
+				rotor1[i][25]=temp;
+			}
+		}
+	}
+	if(rotor==2){
+		for(i=0;i<2;i++){
+			for(j=0;j<amount;j++){
+					temp=rotor2[i][0];
+				for(k=0;k<25;k++){
+					rotor2[i][k]=rotor2[i][k+1];
+					}
+				rotor2[i][25]=temp;
+			}
+		}
+	}
+	if(rotor==3){
+		for(i=0;i<2;i++){
+			for(j=0;j<amount;j++){
+					temp=rotor3[i][0];
+				for(k=0;k<25;k++){
+					rotor3[i][k]=rotor3[i][k+1];
+					}
+				rotor3[i][25]=temp;
+			}
+		}
+	}
+
+}
 
 int position(char input){
 	if(input>96){	//lowercase
